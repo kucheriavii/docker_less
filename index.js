f8931d62fd377b3fce6d002aec4b5f9316288770
kucheriavii <kucheriavii@gmail.com>
2023-08-27 13:43:27 +0300

    14 - Rebuilding Our Image

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ const app = express()
 const port = 3000;
 
 app.get("/", (req, res) => {
-    res.send("Hello world")
+    res.send("Hello world!")
 })
 
 app.listen(port, () => {
     console.log("Now running on PORT " + 3000);
 })
+
